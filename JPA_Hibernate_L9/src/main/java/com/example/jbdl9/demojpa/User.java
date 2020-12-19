265d6aa39ac73e9fd918a795ce517a751ae47956
piyushaggarwal5807 <piyush.a@hike.in>
2020-12-19 20:50:42 +0530

    adding spring data jpa code

---
+++ JPA_Hibernate_L9/src/main/java/com/example/jbdl9/demojpa/User.java
@@ -0,0 +1,60 @@
+package com.example.jbdl9.demojpa;
+
+import org.hibernate.query.criteria.internal.expression.function.AggregationFunction;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName; // -> first_name
+    private String lastName;
+    private int age;
+
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
